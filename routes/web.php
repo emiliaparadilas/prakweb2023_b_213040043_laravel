f5c3c19e93ab339213496a6c8a66ac77bd742e8b
emiliaparadilas <emiliapsupriatna@gmail.com>
2023-10-27 22:25:46 +0700

    3.Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Emilia Paradila S",
+        "email" => "emilia.213040043@mail.unpas.ac.id",
+        "image" => "profile.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
