cf1133a0ed076f6de7296bd6a3592bef9418640f
emiliaparadilas <emiliapsupriatna@gmail.com>
2023-10-28 01:18:23 +0700

    4. Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,70 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Emilia Paradila S",
         "email" => "emilia.213040043@mail.unpas.ac.id",
         "image" => "profile.jpeg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Emilia Paradila S",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo labore corrupti veritatis delectus iure, dolorem molestiae exercitationem recusandae? Animi error in quaerat nesciunt fuga placeat enim temporibus sequi ex assumenda."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Eti",
+            "body" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Emilia Paradila S",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo labore corrupti veritatis delectus iure, dolorem molestiae exercitationem recusandae? Animi error in quaerat nesciunt fuga placeat enim temporibus sequi ex assumenda."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Eti",
+            "body" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"]  === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
