440078f3ec8b7a9cea3f54583284a0350926f646
emiliaparadilas <emiliapsupriatna@gmail.com>
2023-11-03 16:19:12 +0700

    10. Factory & Faker

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use League\CommonMark\Node\Block\Paragraph;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,7 +18,12 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence(mt_rand(2,8)),
+            'slug' => $this->faker->slug(),
+            'excerpt' => $this->faker->paragraph(),
+            'body' => $this->faker->paragraph(mt_rand(5,10)),
+            'user_id' => mt_rand(1,3),
+            'category_id' => mt_rand(1,2)
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,19 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+       
+        // User::create([
+        //     'name' => 'Emilia Paradila S',
+        //     'email' => 'paradilasemilia@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => 'Emilia Paradila S',
-            'email' => 'paradilasemilia@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Putri Azizah',
+        //     'email' => 'azizaputri@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => 'Putri Azizah',
-            'email' => 'azizaputri@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        User::factory(3)->create();
+
 
         Category::create([
             'name' => 'Web Programming',
@@ -39,41 +41,43 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex quis id ad vitae saepe, vel odio excepturi neque aperiam veritatis. Tempora, obcaecati. Asperiores numquam nemo fuga laudantium in error nisi cupiditate laborum dolore sapiente corporis aperiam aspernatur dolorum cumque magnam assumenda perferendis tempore doloribus repellendus dicta fugit, animi necessitatibus quos! Esse ea atque porro sapiente suscipit, id harum ad nemo repellat recusandae, quos laborum odit illo laboriosam, amet cupiditate necessitatibus facilis dolorem ut molestias iusto nulla assumenda. Recusandae nulla, unde minus accusamus odio sint, natus explicabo accusantium quos, consequuntur sunt inventore.',
-            'category_id' => 1, 
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex quis id ad vitae saepe, vel odio excepturi neque aperiam veritatis. Tempora, obcaecati. Asperiores numquam nemo fuga laudantium in error nisi cupiditate laborum dolore sapiente corporis aperiam aspernatur dolorum cumque magnam assumenda perferendis tempore doloribus repellendus dicta fugit, animi necessitatibus quos! Esse ea atque porro sapiente suscipit, id harum ad nemo repellat recusandae, quos laborum odit illo laboriosam, amet cupiditate necessitatibus facilis dolorem ut molestias iusto nulla assumenda. Recusandae nulla, unde minus accusamus odio sint, natus explicabo accusantium quos, consequuntur sunt inventore.',
-            'category_id' => 1, 
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex quis id ad vitae saepe, vel odio excepturi neque aperiam veritatis. Tempora, obcaecati. Asperiores numquam nemo fuga laudantium in error nisi cupiditate laborum dolore sapiente corporis aperiam aspernatur dolorum cumque magnam assumenda perferendis tempore doloribus repellendus dicta fugit, animi necessitatibus quos! Esse ea atque porro sapiente suscipit, id harum ad nemo repellat recusandae, quos laborum odit illo laboriosam, amet cupiditate necessitatibus facilis dolorem ut molestias iusto nulla assumenda. Recusandae nulla, unde minus accusamus odio sint, natus explicabo accusantium quos, consequuntur sunt inventore.',
+        //     'category_id' => 1, 
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex quis id ad vitae saepe, vel odio excepturi neque aperiam veritatis. Tempora, obcaecati. Asperiores numquam nemo fuga laudantium in error nisi cupiditate laborum dolore sapiente corporis aperiam aspernatur dolorum cumque magnam assumenda perferendis tempore doloribus repellendus dicta fugit, animi necessitatibus quos! Esse ea atque porro sapiente suscipit, id harum ad nemo repellat recusandae, quos laborum odit illo laboriosam, amet cupiditate necessitatibus facilis dolorem ut molestias iusto nulla assumenda. Recusandae nulla, unde minus accusamus odio sint, natus explicabo accusantium quos, consequuntur sunt inventore.',
-            'category_id' => 2, 
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex quis id ad vitae saepe, vel odio excepturi neque aperiam veritatis. Tempora, obcaecati. Asperiores numquam nemo fuga laudantium in error nisi cupiditate laborum dolore sapiente corporis aperiam aspernatur dolorum cumque magnam assumenda perferendis tempore doloribus repellendus dicta fugit, animi necessitatibus quos! Esse ea atque porro sapiente suscipit, id harum ad nemo repellat recusandae, quos laborum odit illo laboriosam, amet cupiditate necessitatibus facilis dolorem ut molestias iusto nulla assumenda. Recusandae nulla, unde minus accusamus odio sint, natus explicabo accusantium quos, consequuntur sunt inventore.',
+        //     'category_id' => 1, 
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex quis id ad vitae saepe, vel odio excepturi neque aperiam veritatis. Tempora, obcaecati. Asperiores numquam nemo fuga laudantium in error nisi cupiditate laborum dolore sapiente corporis aperiam aspernatur dolorum cumque magnam assumenda perferendis tempore doloribus repellendus dicta fugit, animi necessitatibus quos! Esse ea atque porro sapiente suscipit, id harum ad nemo repellat recusandae, quos laborum odit illo laboriosam, amet cupiditate necessitatibus facilis dolorem ut molestias iusto nulla assumenda. Recusandae nulla, unde minus accusamus odio sint, natus explicabo accusantium quos, consequuntur sunt inventore.',
-            'category_id' => 2, 
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex quis id ad vitae saepe, vel odio excepturi neque aperiam veritatis. Tempora, obcaecati. Asperiores numquam nemo fuga laudantium in error nisi cupiditate laborum dolore sapiente corporis aperiam aspernatur dolorum cumque magnam assumenda perferendis tempore doloribus repellendus dicta fugit, animi necessitatibus quos! Esse ea atque porro sapiente suscipit, id harum ad nemo repellat recusandae, quos laborum odit illo laboriosam, amet cupiditate necessitatibus facilis dolorem ut molestias iusto nulla assumenda. Recusandae nulla, unde minus accusamus odio sint, natus explicabo accusantium quos, consequuntur sunt inventore.',
+        //     'category_id' => 2, 
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis atque, nostrum amet consequuntur itaque aut et sint ex quis id ad vitae saepe, vel odio excepturi neque aperiam veritatis. Tempora, obcaecati. Asperiores numquam nemo fuga laudantium in error nisi cupiditate laborum dolore sapiente corporis aperiam aspernatur dolorum cumque magnam assumenda perferendis tempore doloribus repellendus dicta fugit, animi necessitatibus quos! Esse ea atque porro sapiente suscipit, id harum ad nemo repellat recusandae, quos laborum odit illo laboriosam, amet cupiditate necessitatibus facilis dolorem ut molestias iusto nulla assumenda. Recusandae nulla, unde minus accusamus odio sint, natus explicabo accusantium quos, consequuntur sunt inventore.',
+        //     'category_id' => 2, 
+        //     'user_id' => 2
+        // ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
