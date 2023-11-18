4bdf4f63b7d361e6527628e5c93c3c5376f9e880
emiliaparadilas <emiliapsupriatna@gmail.com>
2023-11-18 14:51:32 +0700

    20. Update & Delete Post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -5,7 +5,7 @@
 </div>
 
 @if (session()->has('success'))
-<div class="alert alert-success" role="alert">
+<div class="alert alert-success col-lg-8" role="alert">
     {{ session('success') }}
   </div>
 @endif
@@ -29,8 +29,12 @@
               <td>{{$post->category->name }}</td>
               <td>
                 <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><i class="bi bi-eye"></i></a>
-                <a href="" class="badge bg-warning"><i class="bi bi-pencil-square"></i></a>
-                <a href="" class="badge bg-danger"><i class="bi bi-x-circle"></i></a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><i class="bi bi-pencil-square"></i></a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')"><i class="bi bi-x-circle"></i></button>
+                </form>
               </td>
             </tr>
             @endforeach
