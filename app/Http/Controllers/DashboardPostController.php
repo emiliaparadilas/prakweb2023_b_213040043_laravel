d983d6b8b03c14411d3f2f56f4484efb8a123165
emiliaparadilas <emiliapsupriatna@gmail.com>
2023-11-18 13:36:40 +0700

    19. Validation & Insert Post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
 
 class DashboardPostController extends Controller
 {
@@ -34,7 +35,19 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+        $validatetedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body' => 'required'
+        ]);
+
+        $validatetedData['user_id'] = auth()->user()->id;
+        $validatetedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::create($validatetedData);
+
+        return redirect('/dashboard/posts')->with('success', 'New post has been added!');
     }
 
     /**
